361baf93d107750ea24e0965e6bebb9dbf661fda
adria <adria@AdrianPCGamer>
2022-03-26 11:54:12 -0600

    Primer Commit

--- Selenium/src/main/java/test/Selenium.java
+++ Selenium/src/main/java/test/Selenium.java
@@ -9,7 +9,7 @@ public class Selenium {
 		String exePath = "./driver/chromedriver.exe"; //especificar la ruta del chrome driver
 		System.setProperty("webdriver-chrome.driver", exePath); //agregando la ruta del chrome
 		WebDriver driver = new ChromeDriver(); //objeto de webdriver
-		driver.get("https://www.google.com");
+		driver.get("https://www.google.com"); 
 		
 	}
 
