c05edf8042821b522e94ed9d2ca51dc410039bb8
adria <adria@AdrianPCGamer>
2022-03-26 12:02:37 -0600

    prueba de branch

--- Selenium/src/main/java/test/Selenium.java
+++ Selenium/src/main/java/test/Selenium.java
@@ -10,7 +10,7 @@ public class Selenium {
 		System.setProperty("webdriver-chrome.driver", exePath); //agregando la ruta del chrome
 		WebDriver driver = new ChromeDriver(); //objeto de webdriver
 		driver.get("https://www.google.com"); 
-		
+		//una prueba
 	}
 
 }
