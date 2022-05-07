dcff15c7d288764ffdb990acfdc5fb7bb0f528a3
Adrian <adrian@AdrianPCGamer>
2022-05-07 13:21:41 -0500

    asdsad

--- Selenium/src/test/java/test/Selenium_Practicaa.java
+++ Selenium/src/test/java/test/Selenium_Practicaa.java
@@ -5,10 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Selenium_Practica {
+public class Selenium_Practicaa {
 
 	public static void main(String[] args) {
-	//	String exePath = "./driver2.0/chromedriver1.exe"; //especificar la ruta del chrome driver
+		String exePath = "./driver2.0/chromedriver1.exe"; //especificar la ruta del chrome driver
+		System.setProperty("webdriver-chrome.driver", exePath); 
+	//
 		System.setProperty("webdriver-chrome.driver", "C:\\Users\\adria\\Downloads\\chromedriver\\chromedriver.exe"); //agregando la ruta del chrome
 		WebDriver driver = new ChromeDriver(); //objeto de webdriver
 		driver.get("https://opensource-demo.orangehrmlive.com");
